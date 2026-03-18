6a4c0b7170b9747725cc03caa24b75527d02e506
Victor <victor@w3api.com>
2026-03-18 21:23:51 +0100

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 
 export default defineConfig({
+  site: process.env.SITE_URL || 'http://localhost:3000',
   integrations: [expressiveCode()],
 });
